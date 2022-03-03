69c10e83695dfae92d39be6a3acf89dfb49dcccc
Dilafruz <https://github.com/Baffer007/avtoelon.git>
2022-03-03 19:08:31 +0500

    Bu komment

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,7 +6,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
+      drawer: Drawer(
+        backgroundColor: Colors.blue,
+      ),
       appBar: AppBar(
         actions: [
           IconButton(onPressed: () {}, icon: Icon(Icons.search),),
