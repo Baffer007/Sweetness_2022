c3a0fafb6093bcc2fbc219d9b8b258b760681e87
Dilafruz <https://github.com/Baffer007/avtoelon.git>
2022-03-03 19:39:20 +0500

    black ha

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Drawer(
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.blue,
       ),
       appBar: AppBar(
         actions: [
